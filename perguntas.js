4b86f4eb45b0f9fa6423090d763698516966408b
Taconi08 <159157513+Taconi08@users.noreply.github.com>
2024-10-08 12:18:09 -0300

    lala

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao (
+    'Suco',
+    'O suco de vocẽs é natural?',
+    'Sim, todos.'
+
+)
+
+criaCartao (
+    'Suco',
+    'Vocẽs fazem entrega',
+    'Sim.'
+
+)
+
+criaCartao (
+    'Suco',
+    'Qual o melhor suco de Londrina?',
+    'O do Point do Suco.'
+
+)
+
+criaCartao (
+    'Suco',
+    'O quais sabores vocẽs tem?',
+    'Temos laranja, maracujá, uva, goiaba, laranja com morango.'
+
+)
+
+criaCartao (
+    'Suco',
+    'Quanto custa o Litro?',
+    '1l 13,00 - 2l 24,00 - 3l 33,00 - 5l 54,00.'
+
+)
+
+criaCartao (
+    'Suco',
+    'Vocẽs revendem?',
+    'Sim.'
+
+)
